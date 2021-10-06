ed3e183ee918074b223f406871e0933e0a66c89c
woood0207 <woood0207@gmail.com>
2021-10-06 19:45:59 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+function Food(){
+  return <h1> I like Potato </h1>;
+}
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
